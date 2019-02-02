a4b840f7b6bbd5c164ded92d31a4ec81ca53ab2b
Josh VanOverbeke <joshvanoverbeke@gmail.com>
2019-02-01 22:14:42 -0600

    Restyled and repurposed it is now warcraft game, added many sound and png assets and reworked some logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,71 +2,171 @@ $(document).ready(function() {
 
     let wins = 0;
     let losses = 0;
-    let shownNumber;
-    let guessNumber;
+    let maxNumber;
+    let healthNumber;
     let hidden;
+    let enemies = [
+        {
+            id:'murloc',
+            name:'Tinyfin',
+        },
+        {
+            id:'archmage',
+            name:'Archmage',
+        },
+        {
+            id:'deathknight',
+            name:'Death Knight',
+        },
+        {
+            id:'blademaster',
+            name:'Blade Master',
+        },
+        {
+            id:'kotg',
+            name:'Keeper of the Grove',
+        },
+    ]
+    let foe;
     let gameRunning = false;
     let gameOver = false;
-
+    let audio = new Audio('assets/sounds/TheCalm.mp3')
+    // let enemyAudio = new Audio('assets/sounds/'+foe+'.mp3')
 
     //reset the game when the page loads, there is a win, or a loss
     //resets random numbers for the target and the hidden items
     function reset(){
         gameOver = false;
         gameRunning = true;
-        //resets the player number to 0
-        guessNumber = 0;
-        $('#guessNumber').text(guessNumber);
-        console.log(guessNumber);
+        $('#gameOver').hide()
         //generates number from 19-120 that is seen and the player tries to equal
-        shownNumber = Math.floor(Math.random()*102)+19;
-        $('#shownNumber').text(shownNumber);
-        console.log(shownNumber);
+        maxNumber = Math.floor(Math.random()*102)+19;
+        $('#maxNumber').text(maxNumber);
+        console.log(maxNumber);
+            //resets the health to max
+            healthNumber = maxNumber;
+            $('#healthNumber').text(healthNumber);
+            console.log(healthNumber);
         //creates an array of 4 random numbers 1-12
         hidden=[];
         for (let i = 0; i < 4; i++){
             hidden.push(Math.floor(Math.random()*12)+1)
         }
         console.log(hidden);
+        audio.play()
+        updateDisplay()
+    }
+
+    function enemyAppears(foe){
+        $('#foe').attr('src', 'assets/images/'+foe.id+'.png');
+        let enemyAudio = new Audio('assets/sounds/'+foe.id+'.wav');
+        enemyAudio.play()
+        $('#name').text(foe.name);
+    }   
+
+    function updateDisplay(){
+        //set a different foe for ranges of 20, could have had foes appear randomly for random numbers but i wanted to do it this way
+        if (gameRunning === true){
+            $('#foe').show();
+            if (maxNumber <= 40){
+                foe = enemies[0];
+            }
+            else if(maxNumber <= 60){
+                foe = enemies[1];
+            }
+            else if(maxNumber <= 80){
+                foe = enemies[2];
+            }
+            else if(maxNumber <= 100){
+                foe = enemies[3];
+            }
+            else if(maxNumber <= 120){
+                foe = enemies[4];
+            }
+            enemyAppears(foe)
+        }
+        else if (gameOver === true){
+            $('#gameOver').attr('src', 'assets/images/lose.png');
+            $('#gameOver').show();
+        }
+        else{
+
+        } 
     }
 
     //adds the player click to their score and checks if they won or lost
     function addPick(index){
         //adds the click number to the player score
-        guessNumber = guessNumber + hidden[index];
-        console.log(guessNumber);
+        healthNumber = healthNumber - hidden[index];
+        console.log(healthNumber);
         //if player score equals shown number add to wins and reset...
-        $('#guessNumber').text(guessNumber);
-        if(guessNumber === shownNumber){
+        $('#shownNumber').text(healthNumber+'/'+maxNumber);
+        if(healthNumber === 0){
             wins++;
             $('#wins').text('Wins '+ wins);
             //you winnnn!!!
-            gameOver = false;
-            reset()
+            $('#gameOver').attr('src', 'assets/images/victory.png');
+            $('#gameOver').show();
+            $('#start').show();
+            gameOver = true;
         }
         //else if player score is > the shown number add to losses and reset...
-        else if(guessNumber > shownNumber){
+        else if(healthNumber < 0){
             losses++;
             $('#losses').text('Losses '+losses);
-            gameOver = false;
-            reset()
+            $('#gameOver').attr('src', 'assets/images/lose.png');
+            $('#gameOver').show();
+            $('#start').show();
+            gameOver = true;
         }
     }
 
-    //run reset function to ready page for the first game
-    reset()
+    audio.play()
+    $('#gameOver').hide()
+    $('#health').hide()
+    $('#enemy').hide()
+    $('#tracker').hide()
+    $('#foe').hide()
 
+    $('#start').on('click', function(){
+        gameRunning = true;
+        reset();
+        updateDisplay();
+        $('#health').show()
+        $('#enemy').show()
+        $('#tracker').show()
+        $('#shownNumber').text(healthNumber+'/'+maxNumber);
+        $('#start').hide();
+    })
+
+    //randomly selected worker sound function
+    function workerSound(worker){
+        let workerAudio = new Audio('assets/sounds/'+worker+'/'+Math.floor(Math.random()*11)+'.wav');
+        workerAudio.play();
+    }
     // set up onclick event for each picture, referencing a different number from the hidden array
     $('#hidden1').on("click", function(){
-        addPick(0)
+        workerSound('acolyte')
+        if(gameOver === false){
+        addPick(0);
+        }
     });
     $('#hidden2').on("click", function(){
-        addPick(1)
+        workerSound('peon');
+        if(gameOver === false){
+        addPick(1);
+        }
     });
     $('#hidden3').on("click", function(){
-        addPick(2)
+        workerSound('peasant');
+        if(gameOver === false){
+        addPick(2);
+        }
     });
     $('#hidden4').on("click", function(){
-        addPick(3)
+        workerSound('wisp');
+        if(gameOver === false){
+        addPick(3);
+        }
     });
 })
